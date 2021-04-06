c1ac6cf04634d761b8da2875b710c5efd309ce30
Yousaf Azabi <yazabi@fulcrumgt.com>
2021-04-06 15:34:03 +0100

    Add Button UI

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
 import DataTable from './components/DataTable';
+import Button from '@material-ui/core/Button';
+import AddIcon from '@material-ui/icons/Add';
 
 function App() {
   return (
     <div className="App">
+      <Button
+        variant="contained"
+        color="primary"
+        startIcon={<AddIcon />}
+        onClick={}
+      >
+        Book Time
+      </Button>
       <DataTable />
     </div>
   );
