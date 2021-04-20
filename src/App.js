033a3d59433d72e00ba67ec3c98108293fe74f4e
Yousaf Azabi <yazabi@fulcrumgt.com>
2021-04-20 14:51:24 +0100

    add close to snackbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import { SnackbarProvider } from 'notistack';
+import CloseIcon from '@material-ui/icons/Close';
 import Home from './components/Home';
 
 export default function App() {
+  const notistackRef = React.createRef();
+
   return(
-    <SnackbarProvider maxSnack={3}>
+    <SnackbarProvider
+      maxSnack={3}
+      ref={notistackRef}
+      action={key => <CloseIcon onClick={() => notistackRef.current.closeSnackbar(key)} />}
+    >
       <Home />
     </SnackbarProvider>
   );
